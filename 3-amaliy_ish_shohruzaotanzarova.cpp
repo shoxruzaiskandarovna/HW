55150f665eedd0bb7c4e0e8e4493c66c3f4b4d0f
shoxruza <otanazarovashoxruzaa@gmail.com>
2025-11-12 21:37:46 +0300

    darslik

---
+++ 3-amaliy_ish_shohruzaotanzarova.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+#include<studio.h>
+
+using namespace std;
+int main()
+{
+    double,a,b,c,x;
+    cin>>a>>b>>c>>x;
+    double A = 2*x*x*+pow(a+x*x),1/3;
+    double B a*a+b*c;
+    double C = log b+ln c*c;
+    double D = pow(cos(a+pow(x,3)),2);
+    double Z = A/B+c/d
+}
