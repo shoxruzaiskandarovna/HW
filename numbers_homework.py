d9da01e673cdabce1df7ef8b49e731e733e78282
shoxruza <otanazarovashoxruzaa@gmail.com>
2025-11-12 21:30:04 +0300

    Create numbers_homework.py

---
+++ numbers_homework.py
@@ -0,0 +1,20 @@
+# 1-masal kiritilgan sonning kvadrati va kubini chiqaruvchi dastur
+son = int(input("Istalgan son kiriting: "))
+print(f"{son} ning kvadrati {son ** 2} ga teng")
+print(f"{son} ning kubi {son ** 3} ga teng")
+
+
+
+# 2-MASALA: foydalanuvchi yoshini kiritadi, dastur tug‘ilgan yilni chiqaradi
+yosh = int(input("Yoshingiz nechida? "))
+tugilgan_yil = 2025 - yosh
+print(f"Siz {tugilgan_yil} da tug‘ilgansiz")
+
+
+# 3-MASALA ikki son kiritib, ularning amallarini chiqarish
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
