bd138ccbeb87c7ae4ab0ffecd84d1a17bc955590
shoxruza <otanazarovashoxruzaa@gmail.com>
2025-11-12 21:36:18 +0300

    Create 1-ish.cpp

---
+++ 1-ish.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,S;
+    cout<<"a ni kiriing:"<<endl;
+    cin>>a;
+    cout<<"b ni kiriing:"<<endl;
+    cin>>b;
+    S=a+b;
+    cout<<"yig`indi="<<S;
+    return 0;
+}
+
