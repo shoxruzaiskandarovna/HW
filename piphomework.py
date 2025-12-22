9fbe7a61f4fa6d081b02f80404ee72f1d9f6a934
shoxruza <otanazarovashoxruzaa@gmail.com>
2025-12-22 19:30:37 +0300

    Create piphomework.py

---
+++ piphomework.py
@@ -0,0 +1,12 @@
+import requests
+# from bs4 import BeautifulSoup
+#
+# sahifa = "https://daryo.uz/"
+# r = requests.get(sahifa)
+# soup = BeautifulSoup(r.text, 'html.parser')
+# #
+# news = soup.find_all("h3")
+# print(news[0].text)
+
+# news = soup.find_all(class_="post-title")
+# print(news[0].text)
